270bc1d43bb1afc20398951511b0826a1973411a
PABLO MARTÍNEZ GÁLVEZ <pmg943@inlumine.ual.es>
2026-03-09 00:46:55 +0100

    Enhance filtering functionality in Home component to include city and name search

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housing-location/housing-location';
 import { HousingLocationInfo  } from '../housing-location';
 import { Housing } from '../housing';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,15 +11,15 @@ import { Housing } from '../housing';
   template: `
       <section>
         <form>
-          <input type="text" placeholder="Filter by city" />
-          <button class="primary" type="button">Search</button>
+          <input type="text" placeholder="Filter by city" #filter />
+          <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
         </form>
       </section>
       <section class="results">
         <!-- @for (housingLocation of housingLocationList; track $index) {
           <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
         } -->
-        <app-housing-location *ngFor="let housingLocation of housingLocationList" [housingLocation]="housingLocation"></app-housing-location>
+        <app-housing-location *ngFor="let housingLocation of filteredLocationList" [housingLocation]="housingLocation"></app-housing-location>
       </section>
     `,
   styleUrl: './home.css',
@@ -26,11 +27,32 @@ import { Housing } from '../housing';
 export class Home {
   housingLocationList: HousingLocationInfo[] = [];
   housingService: Housing = inject(Housing);
+
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+  filteredLocationList: HousingLocationInfo[] = [];
   
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
 
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
+    if (this.filteredLocationList.length === 0) {
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.name.toLowerCase().includes(text.toLowerCase()),
+    );
+    }
+  }
+
+  
   // readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
     
   // housingLocation: HousingLocationInfo = {
