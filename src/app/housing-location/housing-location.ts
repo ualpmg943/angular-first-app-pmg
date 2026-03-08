bc13e807a8413a88077d49072576b277924b0fac
PABLO MARTÍNEZ GÁLVEZ <pmg943@inlumine.ual.es>
2026-03-08 21:51:42 +0100

    Update Home and HousingLocation components to enhance housing location display and functionality

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -4,7 +4,13 @@ import {HousingLocationInfo} from '../housing-location';
 @Component({
   selector: 'app-housing-location',
   imports: [],
-  template: ` <p>housing-location works!</p> `,
+  template: `
+  <section class="listing"> 
+    <img class="listing-photo" [src]="housingLocation.photo" alt="External photo of {{ housingLocation.name }}"/>
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p> 
+  </section>
+  `, 
   styleUrl: './housing-location.css',
 })
 export class HousingLocation {
