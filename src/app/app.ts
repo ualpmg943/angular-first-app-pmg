209946a19932bb9bab49abc41279e7e602cfb4d7
PABLO MARTÍNEZ GÁLVEZ <pmg943@inlumine.ual.es>
2026-03-08 20:29:56 +0100

    Update app component to include Home component and inline template

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet, Home],
+template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/public/image.png" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home />
+      </section>
+    </main>
+  `,
   styleUrl: './app.css'
 })
 export class App {
