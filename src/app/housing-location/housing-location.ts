bbb98316ae1c42bb61b7cfda57e2ac2ae760b8fd
PABLO MARTÍNEZ GÁLVEZ <pmg943@inlumine.ual.es>
2026-03-08 20:30:21 +0100

    Add HousingLocation component with template, styles, and tests

---
+++ src/app/housing-location/housing-location.ts
@@ -0,0 +1,10 @@
+import {Component, input} from '@angular/core';
+import {HousingLocationInfo} from '../housinglocation';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [],
+  template: ` <p>housing-location works!</p> `,
+  styleUrl: './housing-location.css',
+})
+export class HousingLocation {}
